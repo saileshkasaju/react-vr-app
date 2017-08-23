8392ba4d66bac2190b8b8456e62a48501e9a1177
saileshkasaju <leosailesh@gmail.com>
2017-08-23 15:56:44 +0545

    add styled text

--- index.vr.js
+++ index.vr.js
@@ -4,6 +4,7 @@ import {
   Pano,
   asset,
   View,
+  Text
 } from 'react-vr';
 
 export default class react_vr_app extends React.Component {
@@ -11,15 +12,28 @@ export default class react_vr_app extends React.Component {
     return (
       <View>
         <Pano
-         source={[
-           asset('right.png'),
-           asset('left.png'),
-           asset('up.png'),
-           asset('down.png'),
-           asset('back.jpg'),
-           asset('front.jpg'),
-         ]}
-        />        
+         source={
+           asset('chess-world.jpg')
+         }
+        />
+        <Text
+        style={{
+          transform: [{
+            translateZ: -1
+          }],
+          color: 'lightblue',
+          backgroundColor: '#335',
+          fontSize: 0.1,
+          fontWeight: 300,
+          width: 0.45,
+          height: 0.45,
+          textAlign: 'center',
+          textAlignVertical: 'center',
+          layoutOrigin: [0.5, 0.5]
+        }}
+        >
+          Hello VR
+        </Text>
       </View>
     );
   }
